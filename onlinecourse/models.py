4e47da93a5f1df889d7cfa3ad80f8bfa285b54be
MegaSeaOtter <120525742+MegaSeaOtter@users.noreply.github.com>
2023-03-15 15:46:43 -0700

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -102,18 +102,18 @@ class Enrollment(models.Model):
     # Has question content
     # Other fields and methods you would like to design
 class Question(models.Model):
-    question = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    content = models.TextField
-    grade = models.BooleanField()
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
+    course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=1000)
+    grade = models.IntegerField(default=0)
 
-    # <HINT> A sample model method to calculate if learner get the score of the question
+    def __str__(self):
+        return self.question_text
+
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
+        selected_incorrect = self.choice_set.filter(is_correct=False, id__in=selected_ids).count()
+        if all_answers == (selected_correct - selected_incorrect):
             return True
         else:
             return False
@@ -125,10 +125,15 @@ class Question(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
+
 class Choice(models.Model):
-     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-     content = models.TextField
-     is_correct = models.DecimalField(max_digits = 2, decimal_places = 1)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=1000)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
